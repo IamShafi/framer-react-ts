3a57c6e3a088051c4a11d7e9fb079104e96c4ba9
shafi <salekin68@gmail.com>
2025-07-16 00:19:01 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div className="main">
+      <About />
+    </div>
+  );
+}
+
+export default App;
