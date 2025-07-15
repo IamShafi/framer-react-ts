a71073ebbeba2867e9fd794a47803cc040770965
shafi <salekin68@gmail.com>
2025-07-16 03:04:44 +0600

    fix: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Button from "./ui/Button";
 
 const Header = () => {
   return (
-    <div className="w-full max-w-[1170px] mx-auto flex items-center justify-between gap-10 py-6 px-5">
+    <div className=" relative w-full max-w-[1170px] mx-auto flex items-center justify-between gap-10 py-6 px-5">
       <img
         src="/logo/Logo.png"
         alt="logo"
@@ -25,7 +25,53 @@ const Header = () => {
       {/* Mobile Menu */}
       <div className="block md:hidden">
         {/* Button */}
-        <div className=""></div>
+        <div className="relative">
+          {/* Hamburger SVG */}
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="6"
+              y="9"
+              width="20"
+              height="2.5"
+              rx="1.25"
+              fill="#4D525F"
+            />
+            <rect
+              x="6"
+              y="15"
+              width="20"
+              height="2.5"
+              rx="1.25"
+              fill="#4D525F"
+            />
+            <rect
+              x="6"
+              y="21"
+              width="20"
+              height="2.5"
+              rx="1.25"
+              fill="#4D525F"
+            />
+          </svg>
+        </div>
+      </div>
+      {/* Menu Items */}
+      <div className="absolute top-[100%] right-[5%] w-full max-w-[416px] flex flex-col gap-6 bg-white rounded-2xl p-6 shadow">
+        <div className="cursor-pointer font-urbanist font-[600] text-[18px] text-[#4D525F]">
+          How it Works
+        </div>
+        <div className="cursor-pointer font-urbanist font-[600] text-[18px] text-[#4D525F]">
+          Learn More
+        </div>
+        <div className="cursor-pointer font-urbanist font-[600] text-[18px] text-[#4D525F]">
+          Contact Us
+        </div>
       </div>
     </div>
   );
