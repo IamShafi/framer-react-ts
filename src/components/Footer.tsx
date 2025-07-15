a43827a3e35e1d760439bd57cf17d2bfdc4798d7
shafi <salekin68@gmail.com>
2025-07-16 04:45:47 +0600

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full max-w-[1200px] mx-auto flex items-center justify-between gap-10 py-6 px-5">
+    <div className="w-full max-w-[1200px] mx-auto flex flex-col items-center justify-between gap-10 py-6 px-5">
       <div className="w-full flex flex-col gap-[32px]">
         {/* left vs right */}
         <div className="w-full max-w-[1200px] flex flex-col md:flex-row gap-[30px] justify-between">
@@ -58,6 +58,19 @@ const Footer = () => {
             />
           </div>
         </div>
+      </div>
+      {/* horizontal line */}
+      <div
+        className="w-full border"
+        style={{ borderColor: "rgb(231, 231, 232)" }}
+      ></div>
+      {/* Copyright */}
+      <div className="w-full relative flex justify-end items-center">
+        {/* Blurred background layer */}
+        <div className="absolute right-[33%] bottom-[0%] translate-y-1/2 w-[350px] h-[50px] bg-[#CC9BFF] opacity-30 blur-xl rounded-full z-0"></div>
+        <p className="relative z-10 text-right font-outfit font-normal text-[16px] text-[#4D525F]">
+          ©2023 MonksPay. All rights reserved.
+        </p>
       </div>
     </div>
   );
